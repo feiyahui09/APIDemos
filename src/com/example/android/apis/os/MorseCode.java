3c7eb0331835d51f32260aea0ba14a3fddcbab74
feiyahui09 <1057229972@qq.com>
2013-12-26 10:41:55 +0800

    this is second h

--- src/com/example/android/apis/os/MorseCode.java
+++ src/com/example/android/apis/os/MorseCode.java
@@ -25,8 +25,9 @@ import android.os.Bundle;
 import android.os.Vibrator;
 import android.view.View;
 import android.widget.TextView;
-import com.example.android.apis.R;
 
+import com.example.android.apis.R;
+  
 /**
  * <h3>App that vibrates the vibrator with the Morse Code for a string.</h3>
 
